a90ad490910ed79960e53ba7d44d566be34fdfaf
insanenerd <thelokeshgoel00@gmail.com>
2020-04-28 21:43:11 +0530

    added urls

--- tweetme/tweetme/urls.py
+++ tweetme/tweetme/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from tweets import views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('tweets/<int:tweet_id>',views.tweet_detail_view)
 ]
